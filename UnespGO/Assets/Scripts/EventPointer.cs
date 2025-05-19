cde98999dd50d38980c98bae7268ce25fc9c37a3
Felipe Sanches <felipe.frota.sanches@gmail.com>
2025-05-19 16:58:50 -0300

    Adição de Event Pointers
    Adicionei uns marcadores no mapa muito bonitinhos, scriptei para ficarem rodando como se fossem animados e deixei um input no script Event Pointer para que dê para interagir com os marcadores.

---
+++ UnespGO/Assets/Scripts/EventPointer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EventPointer : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 50f;
+    [SerializeField] private float amplitude = 2.0f;
+    [SerializeField] private float frequency = 0.50f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FloatAndRotatePoiter();
+    }
+
+    void FloatAndRotatePoiter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, (Mathf.Sin(Time.fixedTime*Mathf.PI*frequency) * amplitude) + 15, transform.position.z);
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Pointer clicked");
+    }
+}
